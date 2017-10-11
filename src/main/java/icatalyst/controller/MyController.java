5c751468f688187836bf2d7c111f89876b34285e
9ravi <annan.shankar1991@gmail.com>
2017-10-11 16:40:47 +0530

    samplespring
    bootstrap

---
+++ src/main/java/icatalyst/controller/MyController.java
@@ -0,0 +1,14 @@
+package icatalyst.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MyController {
+   @RequestMapping("/")
+   public ModelAndView showFirst() {
+	ModelAndView obj = new ModelAndView("index");
+	return obj;
+   }
+}
